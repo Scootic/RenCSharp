8f81815657f58eaa28ae113f230b6d0101ce994a
Scootic <bobsywoo@gmail.com>
2025-11-17 22:03:26 -0600

    only change what you want

--- Assets/RenC#/RenC# Scripts/Sequences/Screen_Events/ActorActions/Actor_Expression.cs
+++ Assets/RenC#/RenC# Scripts/Sequences/Screen_Events/ActorActions/Actor_Expression.cs
@@ -11,7 +11,7 @@ namespace RenCSharp.Sequences
     public class Actor_Expression : Screen_Event
     {
         [SerializeField] private Actor actorToAlter;
-        [SerializeField] private int[] visualSpriteIndexes = new int[1];
+        [SerializeField,Tooltip("Putting in -1 means that layer will be skipped when altering expressions.")] private int[] visualSpriteIndexes = new int[1];
         public override void DoShit()
         {
             GameObject spawnt = GameObject.Find(actorToAlter.ActorName);
@@ -20,6 +20,7 @@ namespace RenCSharp.Sequences
                 Image image = spawnt.transform.GetChild(0).GetComponent<Image>();
                 for (int i = 0; i < visualSpriteIndexes.Length; i++) //loop through all sprites and assign thoroughly, only assign visuals to how many we have
                 {
+                    if (visualSpriteIndexes[i] < 0) continue; //means you can put -1 as a visalspriteindex if you want no change
                     image.sprite = actorToAlter.Visuals[i].layer[visualSpriteIndexes[i]];
                     if (i < visualSpriteIndexes.Length - 1) image = image.transform.GetChild(0).GetComponent<Image>(); //grab child for next step
                 }
