33a81befef052832a9f088ca00926062f11af6ab
Scootic <bobsywoo@gmail.com>
2025-11-19 16:27:25 -0600

    i'm so racist

---
+++ Assets/RenC#/RenC# Scripts/DataTypes/History.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace RenCSharp
+{
+    /// <summary>
+    /// Data type for handling histories. Should be saveable.
+    /// </summary>
+    [Serializable]
+    public struct History 
+    {
+        public string[] SpeakerNames;
+        public string[] DialogBoxes;
+
+        public History(string[] speaker, string[] db) 
+        {
+            SpeakerNames = speaker;
+            DialogBoxes = db;
+        }
+    }
+}
