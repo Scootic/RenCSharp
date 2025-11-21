8a67e71e34f6641c233c09c615c237a344bd1689
Scootic <bobsywoo@gmail.com>
2025-11-20 21:11:58 -0600

    please don't troll me

--- Assets/RenC#/RenC# Scripts/DataTypes/SpriteArray.cs
+++ Assets/RenC#/RenC# Scripts/DataTypes/SpriteArray.cs
@@ -4,12 +4,20 @@ using System.Collections.Generic;
 namespace RenCSharp
 {
     /// <summary>
-    /// A wrapper for actors to have a '2D' sprite array in inspector. Thx Obama.
+    /// A wrapper for actors to have a '2D' sprite array in inspector. Thx Obama. Not even an array anymore. This bastard is
+    /// an overglorified dictionary.
     /// </summary>
     [Serializable]
     public struct SpriteArray 
     {
         public Sprite[] layer;
         public string[] visualIDs;
+
+        public Sprite ReturnSprite(string id)
+        {
+            Sprite fellow = null;
+            int i = 0;
+            return fellow;
+        }
     }
 }
