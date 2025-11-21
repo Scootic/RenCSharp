2d5b8985ad500d00755126d32345da9d9a99b83c
Scootic <bobsywoo@gmail.com>
2025-11-21 11:37:18 -0600

    flag conditions

--- Assets/RenC#/RenC# Scripts/Sequences/SequenceTools/Player_Choice.cs
+++ Assets/RenC#/RenC# Scripts/Sequences/SequenceTools/Player_Choice.cs
@@ -7,8 +7,13 @@ namespace RenCSharp.Sequences
     {
         [SerializeField] private string choiceText;
         [SerializeField] private Sequence resultingSequence;
+        [Header("Conditions")]
+        [SerializeField] private bool requireCondition;
+        [SerializeField] private FlagCondition[] conditions;
 
         public Sequence ResultingSequence => resultingSequence;
         public string ChoiceText => choiceText;
+        public bool RequireCondition => requireCondition;
+        public FlagCondition[] Conditions => conditions;
     }
 }
