5db8788c81642ed4a19681988b563abd1a1daf16
Scootic <bobsywoo@gmail.com>
2025-11-30 19:32:47 -0600

    saving power 2000

--- Assets/RenC#/RenC# Scripts/DataTypes/ScreenToken.cs
+++ Assets/RenC#/RenC# Scripts/DataTypes/ScreenToken.cs
@@ -1,9 +1,12 @@
-using UnityEngine;
+using System.Collections.Generic;
+using System;
 
 namespace RenCSharp
 {
+    [Serializable]
     public struct ScreenToken 
     {
-
+        public List<ActorToken> ActiveActors;
+        public string BackgroundAsset, MusicAsset;
     }
 }
