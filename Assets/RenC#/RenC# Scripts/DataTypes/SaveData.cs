4bf5bacae9a9e35a21cece61c807450a840e6308
Scootic <bobsywoo@gmail.com>
2025-11-30 19:32:47 -0600

    saving power 2000

--- Assets/RenC#/RenC# Scripts/DataTypes/SaveData.cs
+++ Assets/RenC#/RenC# Scripts/DataTypes/SaveData.cs
@@ -1,9 +1,13 @@
 
+using System;
+
 namespace RenCSharp
 {
+    [Serializable]
     public struct SaveData 
     {
         public int CurrentScreenIndex;
         public ScreenToken ScreenInformation;
+        public string CurrentSequenceAsset;
     }
 }
--- Assets/RenC#/RenC# Scripts/SaveLoad.cs
+++ Assets/RenC#/RenC# Scripts/SaveLoad.cs
@@ -1,17 +1,29 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 namespace RenCSharp
 {
     public static class SaveLoad
     {
-        public static void Save(string fileName)
+        public static void Save(string fileName, SaveData sd)
         {
-
+            string filePath = Application.persistentDataPath + "/" + fileName + ".sav";
+            FileStream fs = new FileStream(filePath, FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, sd);
+            fs.Close();
         }
 
-        public static bool TryLoad(string fileName, out GameObject go)
+        public static bool TryLoad(string fileName, out SaveData sd)
         {
-            go = null;
+            string filePath = Application.persistentDataPath + "/" + fileName + ".sav";
+            sd = new SaveData();
+            if (!File.Exists(filePath)) return false;
+
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = new FileStream(filePath, FileMode.Open);
+            sd = (SaveData) bf.Deserialize(fs);
             return true;
         }
     }
