fef2d8ca13e7d314c618d339cc4fd89d0f0fe18d
Scootic <bobsywoo@gmail.com>
2025-12-01 18:36:35 -0600

    moved poems to wrok

--- Assets/RenC#/RenC# Scripts/Sequences/Screen_Events/ddlc/Poem.cs
+++ Assets/RenC#/RenC# Scripts/Sequences/Screen_Events/ddlc/Poem.cs
@@ -5,10 +5,10 @@ namespace RenCSharp.Sequences
     [CreateAssetMenu(menuName = "New Poem")]
     public class Poem : ScriptableObject
     {
-        [SerializeField] private Font poemFont;
+        [SerializeField] private TMP_FontAsset poemFont;
         [SerializeField, TextArea(4, 9)] private string poemText;
 
-        public string PoemText;
-        public TMP_FontAsset PoemFont;
+        public string PoemText => poemText;
+        public TMP_FontAsset PoemFont => poemFont;
     }
 }
