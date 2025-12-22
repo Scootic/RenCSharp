9e1871437b3ae61c1c13fcb344c153c0abe8b52a
Scootic <bobsywoo@gmail.com>
2025-12-22 11:27:52 -0600

    improve reading ability

--- Assets/RenC#/RenC# Scripts/EXPERIMENTAL/Animated_Main_Menu.cs
+++ Assets/RenC#/RenC# Scripts/EXPERIMENTAL/Animated_Main_Menu.cs
@@ -9,6 +9,7 @@ namespace EXPERIMENTAL
         [SerializeField,Min(1)] private int animatorIntRange;
         [SerializeField] private string animatorIntParameterName, animatorTriggerParameterName;
         private float desTime, t;
+        private int prevRoll;
         private Animator me;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
@@ -16,6 +17,7 @@ namespace EXPERIMENTAL
             me = GetComponent<Animator>();
             desTime = Random.Range(minTime, maxTime);
             t = 0;
+            prevRoll = animatorIntRange;
         }
 
         // Update is called once per frame
@@ -26,7 +28,10 @@ namespace EXPERIMENTAL
             {
                 t = 0;
                 desTime = Random .Range(minTime, maxTime);
-                me.SetInteger(animatorIntParameterName, Random.Range(0, animatorIntRange));
+                int roll = Random.Range(0, animatorIntRange);
+                while (roll == prevRoll) roll = Random.Range(0, animatorIntRange);
+                prevRoll = roll;
+                me.SetInteger(animatorIntParameterName, roll);
                 me.SetTrigger(animatorTriggerParameterName);
             }
         }
