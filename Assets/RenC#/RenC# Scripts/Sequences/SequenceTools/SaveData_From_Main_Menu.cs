61286c4f94472fd052fae7a0d094e44f96187241
Scootic <bobsywoo@gmail.com>
2025-12-21 19:24:06 -0600

    filthy scrunt

--- Assets/RenC#/RenC# Scripts/Sequences/SequenceTools/SaveData_From_Main_Menu.cs
+++ Assets/RenC#/RenC# Scripts/Sequences/SequenceTools/SaveData_From_Main_Menu.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 namespace RenCSharp.Sequences
 {
     public class SaveData_From_Main_Menu : MonoBehaviour
     {
         public SaveData SD;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void OnEnable()
+        {
+            SceneManager.sceneLoaded += StopSM;
+        }
 
-        // Update is called once per frame
-        void Update()
+        private void OnDisable()
+        {
+            SceneManager.sceneLoaded -= StopSM;
+        }
+        void StopSM(Scene s, LoadSceneMode lsm)
         {
             if (Script_Manager.SM != null)
             {
                 Script_Manager.SM.LoadShit(SD);
+                Debug.Log("Should be loading aan scene!");
                 Object_Factory.RemoveObject(gameObject.name);
             }
         }
