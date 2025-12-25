59852eeab2282551ddb9978aa3badf78fc39e395
Scootic <bobsywoo@gmail.com>
2025-12-25 11:37:38 -0600

    christmasMiracles

--- Assets/RenC#/RenC# Scripts/DataTypes/Audio_Database.cs
+++ Assets/RenC#/RenC# Scripts/DataTypes/Audio_Database.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine.AddressableAssets;
 namespace RenCSharp
 {
     [CreateAssetMenu(menuName = "New Audio Database")]
     public class Audio_Database : ScriptableObject
     {
         [SerializeField] private List<AudioClip> sounds;
-        [SerializeField] private AssetReference myself;
-        public List<AudioClip> Sounds => sounds;
-        public AssetReference Myself => myself;
+        private Dictionary<string, AudioClip> soundDictionary;
+        private void OnValidate()
+        {
+            Debug.Log("Validating Audio DB!");
+            soundDictionary = new();
+            foreach (AudioClip a in sounds)
+            {
+                soundDictionary.Add(a.name, a);
+            }
+            string result = "All Sounds in Dict: + \n";
+            foreach (KeyValuePair<string, AudioClip> kvp in soundDictionary)
+            {
+                result += kvp.Key + "\n";
+            }
+            Debug.Log(result);
+        }
+
+        private void OnEnable()
+        {
+            OnValidate();
+        }
+        public Dictionary<string, AudioClip> Sounds => soundDictionary;
     }
 }
