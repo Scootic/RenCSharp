bf737891dcd0ce16dedd57f6b939f53f554b9a05
Scootic <bobsywoo@gmail.com>
2025-12-27 13:53:53 -0600

    stuff

---
+++ Assets/ddlc/Scripts/FightStuff/Editor/ControlType_Drawer.cs
@@ -0,0 +1,11 @@
+
+namespace RenCSharp.Combat
+{
+    public class ControlType_Drawer : PolymorphicPropertyDrawer<ControlType>
+    {
+        protected override string DropDownMenuName()
+        {
+            return "Choose Control Type";
+        }
+    }
+}
