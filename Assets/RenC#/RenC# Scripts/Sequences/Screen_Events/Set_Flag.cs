0c2c1367fd0b23d0448816d2145ade9c839a6ede
Scootic <bobsywoo@gmail.com>
2026-01-01 12:05:09 -0600

    purrsistent

--- Assets/RenC#/RenC# Scripts/Sequences/Screen_Events/Set_Flag.cs
+++ Assets/RenC#/RenC# Scripts/Sequences/Screen_Events/Set_Flag.cs
@@ -8,11 +8,12 @@ namespace RenCSharp.Sequences
         [SerializeField] private string flagID = "New Flag";
         [SerializeField] private int flagValue = 0;
         [SerializeField, Tooltip("Decides if you override a flag value, or add to it.")] private bool increment = false;
+        [SerializeField,Tooltip("Decides if you are assigning a flag that is per save file, or persistent across all save data.")] private bool persistent = false;
 
         public override void DoShit()
         {
-            if(!increment) Flag_Manager.SetFlag(flagID, flagValue);
-            else Flag_Manager.IncrementFlag(flagID, flagValue);
+            if(!increment) Flag_Manager.SetFlag(flagID, flagValue, persistent);
+            else Flag_Manager.IncrementFlag(flagID, flagValue, persistent);
         }
 
         public override string ToString()
