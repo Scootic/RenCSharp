932cd2e725737066042cc92a0f6b67f26a00dcb1
Scootic <bobsywoo@gmail.com>
2026-01-03 10:48:01 -0600

    FUck YOU BEZIER!

--- Assets/ddlc/Scripts/FightStuff/Projectiles/Boomerang_Projectile.cs
+++ Assets/ddlc/Scripts/FightStuff/Projectiles/Boomerang_Projectile.cs
@@ -7,12 +7,12 @@ namespace RenCSharp.Combat
         [Header("Boomeranging")]
         [SerializeField, Tooltip("How far the boomerang will go, along lifespan duration")] private float distanceFromSpawn;
         [SerializeField] private float arcHeight = 300f;
-        [SerializeField] private AnimationCurve xCurve;
-        [SerializeField] private AnimationCurve yCurve;
+        [SerializeField] private AnimationCurve animateBezCurve;
 
         private float eval;
         private float t;
-        private Vector3 ogPos, endPos, midPos, arcDir;
+        private Vector3[] boundingPositions = new Vector3[4];
+        private Vector3 arcDir;
 
         protected override void OnEnable()
         {
@@ -23,29 +23,21 @@ namespace RenCSharp.Combat
         public override void UpdateMoveDir(Vector3 v3)
         {
             moveDir = v3;
-            ogPos = transform.position;
-            endPos = ogPos + moveDir * distanceFromSpawn;
-            midPos = Vector3.Lerp(ogPos, endPos, 0.5f);
+            boundingPositions[0] = transform.position;
+            boundingPositions[3] = boundingPositions[0] + moveDir * distanceFromSpawn;
             arcDir = Vector3.Cross(moveDir, Vector3.forward);
-            midPos += arcDir * arcHeight;
-            Debug.Log("Boomerang Positions: og" + ogPos + ",mid" +midPos + ",arcdir" + arcDir + ",end" + endPos);
+            boundingPositions[1] = boundingPositions[0] + arcDir * arcHeight;
+            boundingPositions[2] = boundingPositions[3] + arcDir * arcHeight;
+            Debug.Log("Boomerang Positions: Array - " + boundingPositions[0] + "\n" + boundingPositions[1] + "\n"
+                + boundingPositions[2] + "\n" + boundingPositions[3] + "\narcDir: " + arcDir); 
         }
 
         protected override void Update()
         {
             t += Time.deltaTime;
             eval = t / lifetime;
-            Vector3 newFramePos;
-            Debug.Log("BoomerangEval: " + eval);
-            if (eval <= 0.5f)
-            {
-                newFramePos = Vector3.Slerp(midPos, ogPos, 1 - xCurve.Evaluate(eval * 2));
-            }
-            else
-            {
-                newFramePos = Vector3.Slerp(endPos, midPos, 1 - xCurve.Evaluate(eval * 2 - 1));
-            }
-
+            Vector3 newFramePos = TrigHelper.BezPos(boundingPositions, animateBezCurve.Evaluate(eval));
+            Debug.Log("New Frame Pos: " + newFramePos + ", eval: " + eval);
             Vector3 dirToFramePos = newFramePos - transform.position;
             if(dirToFramePos != Vector3.zero) transform.rotation = TrigHelper.GetQuaternion(dirToFramePos);
 
--- Assets/ddlc/Scripts/FightStuff/TrigHelper.cs
+++ Assets/ddlc/Scripts/FightStuff/TrigHelper.cs
@@ -37,5 +37,84 @@ namespace RenCSharp.Combat
         {
             return ogDir - 2 * Vector3.Dot(ogDir, normalDir) * normalDir;
         }
+
+        /// <summary>
+        /// Get the position along a Bezier curve, based on an array of transforms.
+        /// </summary>
+        /// <param name="boundingPoints">The array of transforms that represent the bounding points</param>
+        /// <param name="percent">The percent along the curve.</param>
+        /// <returns>The vector3 that's percent% along the bezier curve.</returns>
+        public static Vector3 BezPos(Transform[] boundingPoints, float percent)
+        {
+            Vector3 posToReturn = Vector3.zero; //default value because Vector3s can't be null for some stupid dumbass reason of doom
+
+            //convert transforms to positions, because duh
+            Vector3[] rawPositions = new Vector3[boundingPoints.Length];
+            for (int i = 0; i < boundingPoints.Length; i++)
+            {
+                rawPositions[i] = boundingPoints[i].position;
+            }
+
+            //do initial step based on the rawPositions array
+            Vector3[] recursiveLayer = new Vector3[rawPositions.Length - 1];
+            for (int i = 0; i < recursiveLayer.Length; i++)
+            {
+                //store the lerped values between n1 and n2, n2 and n3, n3 and n4, etc. until finished.
+                recursiveLayer[i] = Vector3.Lerp(rawPositions[i], rawPositions[i + 1], percent);
+            }
+
+            //once the initial recursiveLayer array has been setup, make a subarray to store the next step in bezier curve.
+            //once subarray is finished, replace og recursiveLayer with the sublayer.
+            //repeat until you are left with a single value.
+            while (recursiveLayer.Length > 1)
+            {
+                Vector3[] subLayer = new Vector3[recursiveLayer.Length - 1];
+                for (int i = 0; i < subLayer.Length; i++)
+                {
+                    subLayer[i] = Vector3.Lerp(recursiveLayer[i], recursiveLayer[i + 1], percent);
+                }
+                recursiveLayer = subLayer;
+            }
+
+            //return the single value found after (potentially) many lerps.
+            posToReturn = recursiveLayer[0];
+            return posToReturn;
+        }
+
+        /// <summary>
+        /// Get the position along a Bezier curve, based on an array of world positions.
+        /// </summary>
+        /// <param name="boundingPoints">The array of world positions that represent the bounding points</param>
+        /// <param name="percent">The percent along the curve.</param>
+        /// <returns>The vector3 that's percent% along the bezier curve.</returns>
+        public static Vector3 BezPos(Vector3[] boundingPoints, float percent)
+        {
+            Vector3 posToReturn = Vector3.zero; //default value because Vector3s can't be null for some stupid dumbass reason of doom
+
+            //do initial step based on the boundingPoints array
+            Vector3[] recursiveLayer = new Vector3[boundingPoints.Length - 1];
+            for (int i = 0; i < recursiveLayer.Length; i++)
+            {
+                //store the lerped values between n1 and n2, n2 and n3, n3 and n4, etc. until finished.
+                recursiveLayer[i] = Vector3.Lerp(boundingPoints[i], boundingPoints[i + 1], percent);
+            }
+
+            //once the initial recursiveLayer array has been setup, make a subarray to store the next step in bezier curve.
+            //once subarray is finished, replace og recursiveLayer with the sublayer.
+            //repeat until you are left with a single value.
+            while (recursiveLayer.Length > 1)
+            {
+                Vector3[] subLayer = new Vector3[recursiveLayer.Length - 1];
+                for (int i = 0; i < subLayer.Length; i++)
+                {
+                    subLayer[i] = Vector3.Lerp(recursiveLayer[i], recursiveLayer[i + 1], percent);
+                }
+                recursiveLayer = subLayer;
+            }
+
+            //return the single value found after (potentially) many lerps.
+            posToReturn = recursiveLayer[0];
+            return posToReturn;
+        }
     }
 }
