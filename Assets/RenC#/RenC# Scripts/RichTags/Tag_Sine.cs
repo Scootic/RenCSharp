4e624e0506ef706219ad5a3d8814e20f3cd59f5b
Scootic <bobsywoo@gmail.com>
2026-01-14 21:16:39 -0600

    in the mourning

--- Assets/RenC#/RenC# Scripts/RichTags/Tag_Sine.cs
+++ Assets/RenC#/RenC# Scripts/RichTags/Tag_Sine.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using EXPERIMENTAL;
 namespace RenCSharp.Tags
 {
     public class Tag_Sine : Base_Tag
@@ -26,6 +27,28 @@ namespace RenCSharp.Tags
                 Debug.LogWarning("SineText couldn't find a stinkin' good speed value!");
             }
         }
+        /// <summary>
+        /// Fuggin' rando AF!!!!
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="maxDistance">!</param>
+        /// <param name="deviation">FLOAT the deviation from rolled positions that are within christ's domain</param>
+        /// <param name="renderedText">guh</param>
+        protected static void NoiseText(TextMeshProUGUI text, string maxDistance, string deviation, string renderedText)
+        {
+            if(float.TryParse(maxDistance, out float maxDist) && float.TryParse(deviation, out float devi))
+            {
+                int startchar = text.text.Length - 1;
+                text.text = text.text.Insert(startchar, renderedText);
+                int endchar = text.text.Length - 1;
+
+                TagRoutineHandler.TRH.StartCoroutine(NoiseTextRoutine(text, startchar, endchar, maxDist, devi, renderedText));
+            }
+            else
+            {
+                Debug.LogWarning("NoiseText couldn't find a good max dist or deviation!");
+            }
+        }
 
         private static IEnumerator SineTextRoutine(TextMeshProUGUI text, float speed, int startchar, int endchar, string idiotrendertext)
         {
@@ -41,7 +64,7 @@ namespace RenCSharp.Tags
                 {
                     TMP_CharacterInfo c = text.textInfo.characterInfo[i];
                     int index = c.vertexIndex;
-                    Vector3 offset = SineWobble(Time.time * speed + (i - startchar) * Mathf.PI * 0.5f);
+                    Vector3 offset = SineWobble(Time.time * speed + (i - startchar) * Mathf.PI * 0.5f); //offset bastards by partial rotations of unit kirkle
                     vertices[index] += offset;
                     vertices[index + 1] += offset;
                     vertices[index + 2] += offset;
@@ -55,9 +78,43 @@ namespace RenCSharp.Tags
             }
         }
 
+        private static IEnumerator NoiseTextRoutine(TextMeshProUGUI text, int startchar, int endchar, float maxDist, float acceptableDeviation, string idtioRend)
+        {
+            Mesh mesh = text.mesh;
+            while(text.text.Length >= endchar)
+            {
+                if (!text.text.Contains(idtioRend)) text.text = text.text.Insert(startchar, idtioRend);
+
+                text.ForceMeshUpdate();
+                Vector3[] verts = mesh.vertices;
+                for(int i = startchar; i <= endchar; i++)
+                {
+                    TMP_CharacterInfo c = text.textInfo.characterInfo[i];
+                    int index = c.vertexIndex;
+                    Vector3 offset = EvilNoise(maxDist, acceptableDeviation);
+                    verts[index] += offset;
+                    verts[index + 1] += offset;
+                    verts[index + 2] += offset;
+                    verts[index + 3] += offset;
+                }
+                mesh.vertices = verts;
+                text.canvasRenderer.SetMesh(mesh);
+
+                yield return null;
+            }
+        }
+
         private static Vector3 SineWobble(float t)
         {
             return new Vector3(Mathf.Cos(t), Mathf.Sin(t) * 5, 0);
+        }
+
+        private static Vector3 EvilNoise(float maximumer, float deviation)
+        {
+            return new Vector3(
+                RandomHelper.NoRepeatRoll("xRandNoise", -maximumer, maximumer, 0.25f),
+                RandomHelper.NoRepeatRoll("yRandNoise", -maximumer, maximumer, 0.25f),
+                0);
         }
     }
 }
