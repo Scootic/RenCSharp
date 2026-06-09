44c070ac9da8544add776c405f3828d4659dbe21
Scootic <bobsywoo@gmail.com>
2026-06-08 21:35:30 -0500

    yeah?

--- Assets/RenC#/RenC# Scripts/Editor/PolymorphicPropertyDrawer.cs
+++ Assets/RenC#/RenC# Scripts/Editor/PolymorphicPropertyDrawer.cs
@@ -33,7 +33,7 @@ namespace RenCSharp
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
-            if(m_SE == null) m_SE = property;
+            m_SE = property;
             Rect dDownRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
             DropDownMenu(dDownRect, property);
             Rect newPos = new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight, position.width, position.height);
